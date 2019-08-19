e9d9f2515406e0e90dd844abcd0066be850f07da
Tom Hewitt <tommhewitt@gmail.com>
2019-08-19 20:37:13 +0100

    Integrate GhostPath script into the Ghost and GameManager scripts

--- Assets/Scripts/Ghosts/Ghost.cs
+++ Assets/Scripts/Ghosts/Ghost.cs
@@ -4,118 +4,112 @@ using Manager;
 
 namespace Ghosts
 {
-    public class Ghost : MonoBehaviour
-    {
-        public bool edible = false;
-        public bool eaten = false;
-        public bool runningHome = false;
-        bool startedFlashCoroutine = false;
+	public class Ghost : MonoBehaviour
+	{
+		public bool edible = false;
+		public bool eaten = false;
+		public bool runningHome = false;
+		private bool startedFlashCoroutine = false;
 
-        public float speed;
-        public float movingSpeed;
-        public float flashingSpeed;
-        public float eatenSpeed;
+		public float speed;
+		public float movingSpeed;
+		public float flashingSpeed;
+		public float eatenSpeed;
 
-        [Header("Pathfinding")]
-        public Transform[] pathNodes;
-        public int currentNode;
+		public GhostPath ghostPath;
+		private Rigidbody rb;
 
-        [Space()]
-        public Material originalColour;
+		public Material originalColour;
 
-        [HideInInspector]
-        public MeshRenderer bodyColour;
+		[HideInInspector]
+		public MeshRenderer bodyColour;
 
-        Rigidbody rb;
+		[HideInInspector]
+		public CapsuleCollider capsuleCollider;
 
-        [HideInInspector]
-        public CapsuleCollider capsuleCollider;
+		void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+			capsuleCollider = GetComponent<CapsuleCollider>();
 
-        void Start()
-        {
-            rb              = GetComponent<Rigidbody>();
-            capsuleCollider = GetComponent<CapsuleCollider>();
+			bodyColour = GameObject.Find(gameObject.name + "/Model/Body").GetComponent<MeshRenderer>();
+		}
 
-            bodyColour = GameObject.Find(gameObject.name + "/Model/Body").GetComponent<MeshRenderer>();
+		void FixedUpdate()
+		{
+			Move();
+		}
 
-            //InvokeRepeating("CheckToResetGhost", 5f, .1f);
-        }
+		void Move()
+		{
+			// If we are not at the current node
+			if (transform.position != ghostPath.GetCurrentWaypoint().position)
+			{
+				// Calculate moving from where we are to the next node
+				Vector3 p = Vector3.MoveTowards(transform.position, ghostPath.GetCurrentWaypoint().position, speed);
 
-        void FixedUpdate()
-        {
-            Move();
-        }
+				// Look at the next node
+				transform.LookAt(ghostPath.GetCurrentWaypoint().position);
 
-        void Move()
-        {
-            // If we are not at the current node
-            if (transform.position != pathNodes[currentNode].position)
-            {
-                // Calculate moving from where we are to the next node
-                Vector3 p = Vector3.MoveTowards(transform.position, pathNodes[currentNode].position, speed);
+				// Move towards the current node
+				rb.MovePosition(p);
+			}
+			else
+			{
+				// Increase the count i.e the next node
+				ghostPath.SetNextWaypoint();
+			}
+		}
 
-                // Look at the next node
-                transform.LookAt(pathNodes[currentNode].position);
+		public void BecomeEdible()
+		{
+			edible = true;
+			speed = flashingSpeed;
 
-                // Move towards the current node
-                rb.MovePosition(p);
-            }
-            else
-            {
-                // Increase the count i.e the next node
-                currentNode = (currentNode + 1) % pathNodes.Length;
-            }
-        }
+			StartCoroutine(Flash(Color.blue, Color.white));
+		}
 
-        public void BecomeEdible()
-        {
-            edible  = true;
-            speed   = flashingSpeed;
+		IEnumerator Flash(Color one, Color two)
+		{
+			StartCoroutine(WaitUntilInEdible());
+			while (edible)
+			{
+				bodyColour.material.color = one;
+				yield return new WaitForSeconds(.2f);
+				bodyColour.material.color = two;
+				yield return new WaitForSeconds(.2f);
+			}
 
-            StartCoroutine(Flash(Color.blue, Color.white));
-        }
+			// Bit of leeway so coroutines dont overlap and lose colour
+			yield return new WaitForSeconds(.1f);
+			ChangeToOriginalColour();
+		}
 
-        IEnumerator Flash(Color one, Color two)
-        {
-            StartCoroutine(WaitUntilInEdible());
-            while (edible)
-            {
-                bodyColour.material.color = one;
-                yield return new WaitForSeconds(.2f);
-                bodyColour.material.color = two;
-                yield return new WaitForSeconds(.2f);
-            }
+		IEnumerator WaitUntilInEdible()
+		{
+			// Wait until we are not able to be eaten again
+			yield return new WaitForSeconds(Constants.POWERUP_DURATION);
 
-            // Bit of leeway so coroutines dont overlap and lose colour
-            yield return new WaitForSeconds(.1f);
-            ChangeToOriginalColour();
-        }
+			edible = false;
+			startedFlashCoroutine = false;
 
-        IEnumerator WaitUntilInEdible()
-        {
-            // Wait until we are not able to be eaten again
-            yield return new WaitForSeconds(Constants.POWERUP_DURATION);        
+			if (!runningHome)
+				speed = movingSpeed;
 
-            edible                  = false;
-            startedFlashCoroutine   = false;
+			AudioManager.instance.Pause("Ghost Edible");
+		}
 
-            if (!runningHome)   
-                speed               = movingSpeed;
+		public void ChangeToOriginalColour()
+		{
+			bodyColour.material = originalColour;
+		}
 
-            AudioManager.instance.Pause("Ghost Edible");
-        }
-
-        public void ChangeToOriginalColour()
-        {
-            bodyColour.material = originalColour;
-        }
-
-        void OnTriggerEnter(Collider o)
-        {
-            if (o.name == "Ghost Home")
-            {
-                GameManager.instance.ResetGhost(this);
-            }
-        }
-    }
+		void OnTriggerEnter(Collider o)
+		{
+			if (o.name == "Ghost Home")
+			{
+				GameManager.instance.ResetGhost(this);
+			}
+		}
+	}
 }
