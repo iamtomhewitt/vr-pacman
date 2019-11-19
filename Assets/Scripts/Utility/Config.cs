85ecc8d5b5dd43c4a0d7e19fe43fe13484389049
Tom Hewitt <tommhewitt@gmail.com>
2019-11-19 20:32:10 +0000

    💻 Add a config class for using a machine based config file

---
+++ Assets/Scripts/Utility/Config.cs
@@ -0,0 +1,31 @@
+﻿using SimpleJSON;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+	[SerializeField] private TextAsset configFile;
+
+	private JSONNode root;
+
+	public static Config instance;
+
+	private void Awake()
+	{
+		root = JSON.Parse(configFile.text);
+
+		if (instance)
+		{
+			DestroyImmediate(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}
+	}
+
+	public JSONNode GetConfig()
+	{
+		return root;
+	}
+}
