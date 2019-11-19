616076ea6663aac91479f3d2bc57c3a042fd142a
Tom Hewitt <tommhewitt@gmail.com>
2019-11-19 20:01:00 +0000

    💻 Replace the highscore download with SimpleJSON

--- Assets/Scripts/Manager/HighscoreManager.cs
+++ Assets/Scripts/Manager/HighscoreManager.cs
@@ -3,20 +3,21 @@ using UnityEngine;
 using Utility;
 using UnityEngine.Networking;
 using System;
+using SimpleJSON;
 
 namespace Manager
 {
 	public class HighscoreManager : MonoBehaviour
-    {
-        private Highscore[] highscoresList;
+	{
+		private Highscore[] highscoresList;
 
 		private Leaderboard leaderboard;
 
-        private const string privateCode    = "hERKFM6pT0qBOb8ZvypNDQmyok7nHxTkWkM2BFIe7hxQ";
-        private const string publicCode     = "5d810f91d1041303ecafee9f";
-        private const string url            = "http://dreamlo.com/lb/";
+		private const string privateCode = "hERKFM6pT0qBOb8ZvypNDQmyok7nHxTkWkM2BFIe7hxQ";
+		private const string publicCode = "5d810f91d1041303ecafee9f";
+		private const string url = "http://dreamlo.com/lb/";
 
-        public static HighscoreManager instance;
+		public static HighscoreManager instance;
 
 		private void Awake()
 		{
@@ -29,92 +30,97 @@ namespace Manager
 				DontDestroyOnLoad(this.gameObject);
 				instance = this;
 			}
-
-			// UploadNewHighscore("Tom (The Developer)", 4500);
 		}
 
 		/// <summary>
 		/// Saves the highscore to PlayerPrefs.
 		/// </summary>
 		public void SaveLocalHighscore(int score)
-        {
-            int currentHighscore = GetLocalHighscore();
+		{
+			int currentHighscore = GetLocalHighscore();
 
-            if (score > currentHighscore)
-            {
-                Debug.Log("New highscore of " + score + "! Saving...");
-                PlayerPrefs.SetInt(Constants.PLAYER_PREFS_HIGHSCORE_KEY, score);
+			if (score > currentHighscore)
+			{
+				Debug.Log("New highscore of " + score + "! Saving...");
+				PlayerPrefs.SetInt(Constants.PLAYER_PREFS_HIGHSCORE_KEY, score);
 
-                // Player has got a new highscore, which hasn't been uploaded yet, so set it to false (0)
-                PlayerPrefs.SetInt(Constants.ALREADY_UPLOADED_KEY, 0);
-            }
-        }
+				// Player has got a new highscore, which hasn't been uploaded yet, so set it to false (0)
+				PlayerPrefs.SetInt(Constants.ALREADY_UPLOADED_KEY, 0);
+			}
+		}
 
-        public int GetLocalHighscore()
-        {
-            return PlayerPrefs.GetInt(Constants.PLAYER_PREFS_HIGHSCORE_KEY);
-        }
+		public int GetLocalHighscore()
+		{
+			return PlayerPrefs.GetInt(Constants.PLAYER_PREFS_HIGHSCORE_KEY);
+		}
 
 		/// <summary>
 		/// Uploads a new highscore to Dreamlo.
 		/// </summary>
-        public void UploadNewHighscore(string username, int score)
-        {
-            StartCoroutine(UploadNewHighscoreRoutine(username, score));
-        }
+		public void UploadNewHighscore(string username, int score)
+		{
+			StartCoroutine(UploadNewHighscoreRoutine(username, score));
+		}
 
 		/// <summary>
 		/// Routine for uploading a highscore to Dreamlo.
 		/// </summary>
-        private IEnumerator UploadNewHighscoreRoutine(string username, int score)
-        {
+		private IEnumerator UploadNewHighscoreRoutine(string username, int score)
+		{
 			UnityWebRequest request = UnityWebRequest.Post(url + privateCode + "/add/" + username + "/" + score, "");
-            yield return request.SendWebRequest();
+			yield return request.SendWebRequest();
 
 			if (!request.downloadHandler.text.StartsWith("ERROR"))
 			{
-                Debug.Log("Upload successful! " + request.responseCode);
-                PlayerPrefs.SetInt(Constants.ALREADY_UPLOADED_KEY, 1);
-            }
-            else
-            {
-                Debug.Log("Error uploading: " + request.downloadHandler.text);
+				Debug.Log("Upload successful! " + request.responseCode);
+				PlayerPrefs.SetInt(Constants.ALREADY_UPLOADED_KEY, 1);
+			}
+			else
+			{
+				Debug.Log("Error uploading: " + request.downloadHandler.text);
 				FindObjectOfType<HighscoreDisplayHelper>().ClearEntries();
 				FindObjectOfType<HighscoreDisplayHelper>().DisplayError("Could not upload score. Please try again later.\n\n" + request.downloadHandler.text);
-            }
-        }
+			}
+		}
 
 		public Highscore[] GetHighscoresList()
 		{
 			return highscoresList;
 		}
 
+		public void DownloadHighscores()
+		{
+			StartCoroutine(DownloadHighscoresRoutine());
+		}
+
 		/// <summary>
 		/// Downloads the highscores from Dreamlo.
 		/// </summary>
-        public IEnumerator DownloadHighscores()
-        {
-            if (Application.internetReachability == NetworkReachability.NotReachable)
-            {
-				FindObjectOfType<HighscoreDisplayHelper>().DisplayError("No internet connection.");
-                yield break;
-            }
+		private IEnumerator DownloadHighscoresRoutine()
+		{
+			HighscoreDisplayHelper displayHelper = FindObjectOfType<HighscoreDisplayHelper>();
+
+			if (Application.internetReachability == NetworkReachability.NotReachable)
+			{
+				displayHelper.DisplayError("No internet connection.");
+				yield break;
+			}
 
 			UnityWebRequest request = UnityWebRequest.Get(url + publicCode + "/json/0/10");
 			yield return request.SendWebRequest();
 
 			if (!request.downloadHandler.text.StartsWith("ERROR"))
 			{
-				string json = JsonHelper.StripParentFromJson(request.downloadHandler.text, 2);
-				leaderboard = JsonUtility.FromJson<Leaderboard>(json);
-				highscoresList = ToHighScoreList();
+				JSONNode json = JSON.Parse(request.downloadHandler.text);
+				JSONArray entries = json["dreamlo"]["leaderboard"]["entry"].AsArray;
+				displayHelper.DisplayHighscores(entries);
 			}
 			else
 			{
 				Debug.Log("Error downloading: " + request.downloadHandler.text);
-				FindObjectOfType<HighscoreDisplayHelper>().DisplayError("Could not download highscores. Please try again later.\n\n" + request.downloadHandler.text);
+				displayHelper.DisplayError("Could not download highscores. Please try again later.\n\n" + request.downloadHandler.text);
 			}
-        }
+		}
 
 		/// <summary>
 		/// Converts the leaderboard object created by a Json request into a highscore list.
@@ -132,22 +138,22 @@ namespace Manager
 
 			return list;
 		}
-    }
+	}
 
 	/// <summary>
 	/// Data class for a highscore entry.
 	/// </summary>
-    public struct Highscore
-    {
-        public string username;
-        public int score;
+	public struct Highscore
+	{
+		public string username;
+		public int score;
 
-        public Highscore(string username, int score)
-        {
-            this.username = username;
-            this.score = score;
-        }
-    }
+		public Highscore(string username, int score)
+		{
+			this.username = username;
+			this.score = score;
+		}
+	}
 
 	/// <summary>
 	/// Data classes used to map the JSON response onto variables.
