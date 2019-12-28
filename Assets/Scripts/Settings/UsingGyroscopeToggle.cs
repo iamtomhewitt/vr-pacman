6f8f71b86e3142560b9d25937498450f8bf1cf9c
Tom Hewitt <tommhewitt@gmail.com>
2019-12-28 20:30:18 +0000

    💻 Update the toggle so that the relevant settings are shown

--- Assets/Scripts/Settings/UsingGyroscopeToggle.cs
+++ Assets/Scripts/Settings/UsingGyroscopeToggle.cs
@@ -9,12 +9,25 @@ namespace Setting
 	/// </summary>
 	public class UsingGyroscopeToggle : MonoBehaviour
 	{
+		[SerializeField] private GameObject accelerometerSettings;
+		[SerializeField] private GameObject gyroscopeSettings;
+
 		/// <summary>
 		/// Called from a Toggle.
 		/// </summary>
 		public void SetUsingGyro(Toggle toggle)
 		{
 			GameSettingsManager.instance.SetUsingGyro(toggle.isOn);
+		}
+
+		/// <summary>
+		/// Shows the correct settings based on whether we are using gyroscope.
+		/// </summary>
+		public void ShowSettings()
+		{
+			bool usingGyro = GameSettingsManager.instance.IsUsingGyro();
+			gyroscopeSettings.SetActive(usingGyro);
+			accelerometerSettings.SetActive(!usingGyro);
 		}
 	}
 }
