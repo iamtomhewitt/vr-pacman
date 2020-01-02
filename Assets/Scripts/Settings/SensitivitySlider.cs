ede84c1524300c1f552aed90d86fd2b7fdd1ffe5
Tom Hewitt <tommhewitt@gmail.com>
2020-01-02 16:41:42 +0000

    💻 Update references to accelerometer

--- Assets/Scripts/Settings/SensitivitySlider.cs
+++ Assets/Scripts/Settings/SensitivitySlider.cs
@@ -5,21 +5,21 @@ using Manager;
 namespace Settings
 {
 	/// <summary>
-	/// A script to control behaviours from the accelerometer sensitivity slider on the settings scene.
+	/// A script to control behaviours from the sensitivity slider on the settings scene.
 	/// </summary>
-	public class AccelerometerSlider : MonoBehaviour
+	public class SensitivitySlider : MonoBehaviour
 	{
 		private void Start()
 		{
 			Slider slider = GetComponent<Slider>();
 			slider.value = GameSettingsManager.instance.GetSensitivity();
-			UpdateAccelerometerSettings(slider); 
+			UpdateSensitivityInGameSettings(slider); 
 		}
 
 		/// <summary>
 		/// Called from a Slider.
 		/// </summary>
-		public void UpdateAccelerometerSettings(Slider slider)
+		public void UpdateSensitivityInGameSettings(Slider slider)
 		{
 			GameSettingsManager.instance.SetSensitivity(slider.value);
 		}
