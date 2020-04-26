5b4b4649bedd2ba3e324a15e15abc1140b0f4063
Tom Hewitt <tommhewitt@gmail.com>
2020-04-26 16:03:43 +0100

    🎨 Make a colour pattern if the username is me

--- Assets/Scripts/Utility/Highscore/HighscoreEntry.cs
+++ Assets/Scripts/Utility/Highscore/HighscoreEntry.cs
@@ -7,17 +7,41 @@ using UnityEngine.UI;
 /// </summary>
 namespace Utility
 {
-	public class HighscoreEntry : MonoBehaviour
-	{
-		[SerializeField] private Text rank;
-		[SerializeField] private Text username;
-		[SerializeField] private Text score;
+    public class HighscoreEntry : MonoBehaviour
+    {
+        [SerializeField] private Text rank;
+        [SerializeField] private Text username;
+        [SerializeField] private Text score;
 
-		public void Populate(string rank, string name, string score)
-		{
-			this.rank.text = rank;
-			this.username.text = name;
-			this.score.text = score;
-		}
-	}
+        private string[] devColours = new string[] { "#FF3132", "#00BDDF" };
+
+        public void Populate(string rank, string name, string score)
+        {
+            this.rank.text = rank;
+            this.username.text = name;
+            this.score.text = score;
+
+            if (name.Equals("Tom (The Developer)"))
+            {
+                username.text = ApplyDevColours(name);
+            }
+        }
+
+        private string ApplyDevColours(string username)
+        {
+            string newUsername = "";
+            int index = 0;
+            foreach (char c in username)
+            {
+                newUsername += ColourCharacter(c, devColours[index]);
+                if (c != ' ') index = ++index > devColours.Length - 1 ? 0 : index;
+            }
+            return newUsername;
+        }
+
+        private string ColourCharacter(char s, string colour)
+        {
+            return "<color=" + colour + ">" + s + "</color>";
+        }
+    }
 }
