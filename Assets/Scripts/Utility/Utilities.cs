cb26b13e7172ff891a6b480985bdfc997b8fd2da
Tom Hewitt <tommhewitt@gmail.com>
2020-04-27 16:45:21 +0100

    💻 Add a utility method for stripping out invalid characters that cannot be rendered by the font

--- Assets/Scripts/Utility/Utilities.cs
+++ Assets/Scripts/Utility/Utilities.cs
@@ -47,5 +47,22 @@ namespace Utility
 		{
 			Screen.orientation = ScreenOrientation.Portrait;
 		}
+
+		public static string StripNonLatinLetters(string name)
+		{
+			string newName = "";
+			foreach (char c in name)
+			{
+				int code = (int)c;
+				
+				// Only english numbers, letters, symbols
+				if ((code >= 32 && code <= 127))
+				{
+					newName += c;
+				}
+			}
+
+			return newName;
+		}
 	}
 }
