a571eca8b20eac045d582742f5a4491c71c14a48
Tom Hewitt <tommhewitt@gmail.com>
2020-04-27 16:50:48 +0100

    💻 Remove invalid characters from name when uploading highscores

--- Assets/Scripts/Utility/Highscore/HighscoreDisplayHelper.cs
+++ Assets/Scripts/Utility/Highscore/HighscoreDisplayHelper.cs
@@ -72,6 +72,8 @@ namespace Utility
         {
             Text placeholderText = usernameInputField.placeholder.GetComponent<Text>();
 
+			string formatted = Utilities.StripNonLatinLetters(usernameInputField.text);
+
             if (HighscoreManager.instance.GetLocalHighscore() <= 0)
             {
                 placeholderText.text = Constants.SCORE_NOT_ZERO;
@@ -85,6 +87,11 @@ namespace Utility
                 usernameInputField.text = "";
                 placeholderText.text = Constants.ALREADY_UPLOADED;
             }
+			else if (string.IsNullOrEmpty(formatted))
+			{
+				usernameInputField.text = "";
+				placeholderText.text = Constants.INVALID_NAME;
+			}
             else
             {
                 HighscoreManager.instance.UploadNewHighscore(usernameInputField.text, HighscoreManager.instance.GetLocalHighscore());
