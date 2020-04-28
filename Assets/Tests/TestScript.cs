0bc5a3f87b273b953f01860764c27be0247713da
Tom Hewitt <tommhewitt@gmail.com>
2020-04-28 15:05:08 +0100

    🔧 Create test assembly definition and example test

---
+++ Assets/Tests/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.TestTools;
+using UnityEngine;
+
+namespace Tests
+{
+    public class TestScript
+    {
+        [Test]
+		public void Test()
+		{
+			string s = "abc";
+
+			Assert.AreEqual("abc", s);
+		}
+    }
+}
