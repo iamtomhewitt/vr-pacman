d198e5312773b64cf60ef07c070e2318ece4e302
Tom Hewitt <tommhewitt@gmail.com>
2020-11-12 19:55:48 +0000

    feat(#116): add a teleporter script

---
+++ Assets/Scripts/Environment/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+	[SerializeField] private Transform teleportTo;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag.Equals(Tags.PLAYER))
+		{
+			other.gameObject.transform.position = teleportTo.position;
+			other.gameObject.transform.rotation = teleportTo.rotation;
+		}
+	}
+}
+
+
