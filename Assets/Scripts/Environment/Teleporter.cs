b4ee1ea1fbe5b265bdd0d9283d374fc158ccae1d
Tom Hewitt <tommhewitt@gmail.com>
2020-11-13 13:05:25 +0000

    refactor(#116): update teleporter

--- Assets/Scripts/Environment/Teleporter.cs
+++ Assets/Scripts/Environment/Teleporter.cs
@@ -8,7 +8,9 @@ public class Teleporter : MonoBehaviour
 	{
 		if (other.gameObject.tag.Equals(Tags.PLAYER))
 		{
-			other.gameObject.transform.position = teleportTo.position;
+			Vector3 position = new Vector3(teleportTo.position.x, other.transform.position.y, teleportTo.position.z);
+
+			other.gameObject.transform.position= position;
 			other.gameObject.transform.rotation = teleportTo.rotation;
 		}
 	}
