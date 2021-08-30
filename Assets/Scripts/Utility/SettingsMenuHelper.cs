727628832c6d46b56ddb762ed2e1c794880fd013
Tom Hewitt <tommhewitt@gmail.com>
2021-08-30 11:56:49 +0100

    feat(#133): add halloween to settings menu

---
+++ Assets/Scripts/Utility/SettingsMenuHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Utility;
+
+/// <summary>
+/// Helper class for actions performed in the main menu.
+/// </summary>
+public class SettingsMenuHelper : MonoBehaviour
+{
+	[SerializeField] private GameObject halloweenUi;
+	[SerializeField] private GameObject normalParticleSystem;
+
+	private void Start()
+	{
+		if (Utilities.isOctober())
+		{
+			halloweenUi.SetActive(true);
+			normalParticleSystem.SetActive(false);
+		}
+	}
+}
